bc31c698db49ac0cbead5ceac7991ff2d2aa34a4
LFE <lfe999@protonmail.com>
2020-01-16 22:48:53 -0800

    Person UI Tabs all added

--- Extensions/AtomExtensions.cs
+++ Extensions/AtomExtensions.cs
@@ -26,19 +26,49 @@ namespace LFE.KeyboardShortcuts.Extensions
             switch(atom.type)
             {
                 case "Person":
+                    //first column
                     yield return "Clothing";
                     yield return "Clothing Presets";
                     yield return "Hair";
+                    yield return "Hair Presets";
                     yield return "Male Morphs";
                     yield return "Female Morphs";
-                    yield return "Skin Textures";
-                    yield return "Iris Materials";
+                    yield return "Morphs Presets";
+
+                    yield return "Mouth Materials";
+                    yield return "Tongue Materials";
+                    yield return "Teeth Materials";
+                    yield return "M Eyelash Materials";
+                    yield return "F Eyelash Materials";
+                    yield return "Lacrimals Materials";
                     yield return "Sclera Materials";
-                    yield return "Auto Behaviors";
-                    yield return "Collision Triggers";
+                    yield return "Iris Materials";
+                    yield return "Skin Textures";
+                    yield return "Skin Materials 2";
+                    yield return "Skin Materials 1";
+                    yield return "Skin Select";
+                    yield return "Skin Presets";
+
+                    //second column
+                    yield return "Hand Control";
+                    yield return "Head Audio";
                     yield return "Plugins";
+                    yield return "Auto Behaviours";
+
+                    yield return "Collision Triggers";
+                    yield return "Misc Physics";
+                    yield return "M Pectoral Physics";
+                    yield return "F Breast Physics 2";
+                    yield return "F Breast Physics 1";
+                    yield return "F Breast Presets";
+                    yield return "F Glute Physics";
+                    yield return "F Glute Presets";
                     yield return "Animation";
                     yield return "Move";
+                    yield return "Pose Presets";
+
+                    yield return "Appearance Presets";
+                    yield return "General Presets";
                     yield return "Control";
                     break;
                 default:
